f0f8b6d96293f0adf335f455503f64060b3fac93
Moses583 <moisendungu@gmail.com>
2024-05-22 08:20:46 +0300

    Initial commit

--- app/src/main/java/com/example/jetpackcomposeone/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeone/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.jetpackcomposeone
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,30 +20,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetPackComposeOneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetPackComposeOneTheme {
-        Greeting("Android")
+fun MainScreen(){
+    Button(onClick = { }) {
+        Text(text = "press me")
     }
 }
