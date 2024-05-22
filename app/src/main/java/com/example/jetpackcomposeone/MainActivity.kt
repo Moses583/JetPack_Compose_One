5e7e1305176452495eb78cb722ee7d584b052275
Moses583 <moisendungu@gmail.com>
2024-05-22 08:41:46 +0300

    arrangements and alignments

--- app/src/main/java/com/example/jetpackcomposeone/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeone/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.jetpackcomposeone
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeone.ui.theme.JetPackComposeOneTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +32,27 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun MainScreen(){
-    Button(onClick = { }) {
-        Text(text = "press me")
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = { },
+                modifier = Modifier.padding(5.dp)) {
+                Text(text = "Decrement")
+            }
+            Text(text = "0")
+            Button(onClick = { }) {
+                Text(text = "Increment")
+            }
+        }
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreView(){
+    MainScreen()
 }
